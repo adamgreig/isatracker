c6075f1ac22d70f8e1be1411e9014396bc08c978
Adam Greig <random@randomskk.net>
2010-03-21 15:06:34 +0000

    Renamed get_latest_price to fetch_latest_price for clarity

--- funds/models.py
+++ funds/models.py
@@ -6,7 +6,7 @@ import BeautifulSoup
 class Fund(models.Model):
     name = models.CharField(max_length=100)
     url = models.URLField("URL", verify_exists=False)
-    def get_latest_price(self):
+    def fetch_latest_price(self):
         pass
     def __unicode__(self):
         return self.name
@@ -20,7 +20,7 @@ class FundPrice(models.Model):
 
 class FidelityFund(Fund):
     isin = models.CharField("ISIN", max_length=12)
-    def get_latest_price(self):
+    def fetch_latest_price(self):
         page = urllib2.urlopen(self.url)
         soup = BeautifulSoup.BeautifulSoup(page)
         return float(soup.find("span", id="buyprice").string)
